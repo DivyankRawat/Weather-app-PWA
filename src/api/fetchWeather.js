640848064e664ab5adeb09f9ddfc4e7657886c36
Divyank <divyankrawat10@gmail.com>
2022-02-18 13:31:13 +0530

    React Stuff Finished

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+
+// Here query is city name
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+
+    return data;
+}
+
+
+// only data is needed, not the whole response. So it is being destructured and returned to be used later.
