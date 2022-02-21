1641317dfbe19f9b9e6fb0bf85db9fe4ecf12f7d
Divyank <divyankrawat10@gmail.com>
2022-02-22 01:02:25 +0530

    Hiding Api key

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+const API_KEY = process.env.REACT_APP_API;
 
 // Here query is city name
 export const fetchWeather = async (query) => {
